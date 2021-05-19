4444f2d5f0c0c374367a323c441de4bb37170b0f
Cieluu <russogilles5@gmail.com>
2021-05-19 11:36:56 +0200

    git soll die Projektführung übernehmen

---
+++ src/main/java/ch/bbw/gr/App.java
@@ -0,0 +1,13 @@
+package ch.bbw.gr;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
